bb949482fe9f6593a0b3fcf57328c058234739ce
leonardo <leonardo.fsilva64@senacsp.edu.br>
2023-09-24 09:20:01 -0300

    Site

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # portfolio-senac-web
 Site de portfólio pessoal. Projeto de disciplina de PW do Centro Universitário SENAC
 
+DEV's: Leonardo Feitosa da Silva
+Danilo Barbosa
+
 ## Tarefas
 
 - [x] Header 
 - [x] Apresentação
 - [x] Projetos
-- [x] Orçamento
+- [x] Formulário
 - [ ] Menu
 - [ ] Responsividade
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,19 @@
 //pegar os valores do formulário
 let nome = document.querySelector("#nome")
 
-const campoQtde = document.querySelector("#qtde")
-const resultado = document.querySelector("output")
-const campoTipo = document.querySelector("#tipo")
-const campoLayoutSim = document.querySelector("#layout_sim")
-const campoLayoutNao = document.querySelector("#layout_nao")
 const campoJS = document.querySelector("#js")
 const campoPrazo = document.querySelector("#prazo")
+const resultado = document.querySelector("output")
+
+campoJS.addEventListener("change", bemvindo)
 
 campoPrazo.addEventListener("input", function(){
     const labelPrazo = document.querySelector("label[for='prazo']")
-    labelPrazo.innerHTML =`Prazo:  ${campoPrazo.value}  semanas`
+    labelPrazo.innerHTML =`Probabilidade:  ${campoPrazo.value}  %`
     calcular()
 })
 
-campoQtde.addEventListener("change", calcular)
-campoTipo.addEventListener("change", calcular)
-campoLayoutSim.addEventListener("change", calcular)
-campoLayoutNao.addEventListener("change", calcular)
-campoJS.addEventListener("change", calcular)
-
-
-// calcular o preço
-function calcular(){
-    //R$100,00 por pagina
-   let qtde = campoQtde.value
-   let valor = qtde*100
-    if(campoTipo.value==2) valor += 1000
-
-    if(campoLayoutSim.checked)valor += qtde * 50
-
-    if(campoJS.checked)valor *= 1.1
-
-    let taxaDeUrgencia = 1 - campoPrazo.value * 0.05
-    valor *= 1 + taxaDeUrgencia
-
-        resultado.innerText = `R$${valor}`
+function bemvindo(){
+    if(campoJS.checked) resultado.innerText =`Bem vindo! Volte Sempre.`
+    else resultado.innerText = ``
 }
-
-// mostrar o preço
